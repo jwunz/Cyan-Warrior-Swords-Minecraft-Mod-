25bef49bf3b5b914613590520784cdbc7e942c1a
Joseph Wunz <jodaman19@gmail.com>
2017-11-13 16:29:39 -0700

    Created Package com/cyanwarriorswords/cyanwarriorswordmod/Swords/TNTType and Class ItemTNTSword.java

---
+++ com/cyanwarriorswords/cyanwarriorswordmod/Swords/TNTType/ItemTNTSword.java
@@ -0,0 +1,12 @@
+package com.cyanwarriorswords.cyanwarriorswordmod.Swords.TNTType;
+
+import net.minecraft.item.ItemSword;
+
+public class ItemTNTSword extends ItemSword {
+
+	public ItemTNTSword(ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
