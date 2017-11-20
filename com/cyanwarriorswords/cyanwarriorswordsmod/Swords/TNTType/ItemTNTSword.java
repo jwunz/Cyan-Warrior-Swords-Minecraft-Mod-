2506817e855571f2eb05b3c08360f6a7e0743998
jddrago <jason@delorie.com>
2017-11-20 15:05:12 -0700

    created CannonSword class and added in the code to register our items with forge

--- com/cyanwarriorswords/cyanwarriorswordsmod/Swords/TNTType/ItemTNTSword.java
+++ com/cyanwarriorswords/cyanwarriorswordsmod/Swords/TNTType/ItemTNTSword.java
@@ -1,12 +1,22 @@
 package com.cyanwarriorswords.cyanwarriorswordsmod.Swords.TNTType;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemSword;
 
 public class ItemTNTSword extends ItemSword {
 
-	public ItemTNTSword(ToolMaterial p_i45356_1_) {
+	public ItemTNTSword(int par1, ToolMaterial p_i45356_1_) {
 		super(p_i45356_1_);
+		this.setMaxDamage(1000);
+		this.setMaxStackSize(1);
 		// TODO Auto-generated constructor stub
 	}
 
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister ir)
+	{  
+	        this.itemIcon = ir.registerIcon("CyanWarriorSwords:TNTSword");
+	}
 }
